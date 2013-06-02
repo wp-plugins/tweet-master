cae5a71452f09078a83383d7e1d8065e8800849b
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-02 19:35:01 +0000

    version 2.0 tags
    git-svn-id: https://plugins.svn.wordpress.org/tweet-master/tags/2.0@721734 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ techgasp-tweetmaster-admin.php
@@ -0,0 +1,56 @@
+<div class="wrap">
+<div class="icon32" style="width:40px; vertical-align:middle;"><img src="../wp-content/plugins/tweet-master/techgasp-minilogo.png" alt="' . esc_attr__( 'TechGasp Plugins') . '" /><br /></div>
+
+<h2><b>TechGasp - Tweet Master Lite Version</b></h2>
+<form method="post" action="techgasp-tweetnmaster-admin.php"> 
+<p><b>About TechGasp:</b></p>
+<p>Major player in the Content Management System world! Joomla and Jomsocial Partner with more than 100 high quality, error free Extensions. We provide website customizations and development, SEO Optimization, Facebook Apps, etc. We have fast & furious specialized Hosting for WordPress and Joomla. Our promise, the use of minimal error free code with fast loading times. Also to avoid the implementation of useless Javascript and Ajax in CMS by replacing it with solida php, css, html5 and iframe. Check our website for more high quality extensions. Stay up to date by "like" and "follow" our facebook and twitter page for fresh news, releases and upgrades and updates.</p>
+
+<p>
+<a class="button-primary" href="http://wordpress.techgasp.com" target="_blank" title="Visit Website">Wordpress Website</a> 
+<a class="button-primary" href="http://www.techgasp.com" target="_blank" title="Visit Website">Joomla Website</a>
+<a class="button-primary" href="http://hosting.techgasp.com" target="_blank" title="Visit Website">Hosting Website</a>
+<a class="button-secondary" href="https://www.facebook.com/TechGasp" target="_blank" title="Facebook Page">Facebook Page</a> 
+<a class="button-secondary" href="https://twitter.com/TechGasp" target="_blank" title="Follow Twitter">Twitter Page</a>
+<a class="button-secondary" href="https://plus.google.com/118126459543511361864" target="_blank" title="Follow Google">Google Page</a>
+</p>
+
+<h3>Stay up-to-date with new extension releases, extension updates, & upgrades:</h3>
+<p>
+<span style="float:left; vertical-align:baseline;">
+<fb:like href="https://www.facebook.com/TechGasp" send="true" layout="button_count" width="90" show_faces="false"></fb:like>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=281766848505812";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+&nbsp;
+<a href="https://twitter.com/TechGasp" class="twitter-follow-button" data-show-count="true">Follow @TechGasp</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</span>
+</p>
+
+<hr>
+
+<img src="../wp-content/plugins/tweet-master/techgasp-tweetmaster-logo.png" alt="Tweet Master" align="left" width="387px" height="171px" style="padding:5px;"/>
+<p><h2>Tweet Master:</h2></p>
+<p>With Tweet Master plugin you can display your latest tweets and tweet button inside any widget position of your wordpress template.</p>
+</br>
+<p><em>Tweet Master boosts your social engagemen...</em><p>
+</br>
+</br>
+<p><h3>Options & Configuration:</h3></p>
+<p><b>All configuration options are available in your widgets page. Tweet Master Widget:</b></p>
+<p>Select or <em>"add"</em > Tweet Master Widget. Configure your settings. Select widget position according to your template. That easy...</p>
+<p><b>Insert Tweet Master Widget inside Pages and Posts (advanced version only):</b></p>
+<p>Copy and Paste this shortcode inside your pages or posts [tweetmaster id=xx] replace xx with your sidebar number, ie [tweetmaster id=sidebar-1]</p>
+<p><a class="button-primary" href="widgets.php" title="Widgets Page">To Widgets Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/tweet-master/" target="_blank" title="More Help">More Help</a> <a class="button-primary" href="http://wordpress.techgasp.com/tweet-master/" target="_blank" title="Tweet Master Advanced Version">Tweet Master Advanced Version</a></p>
+<hr>
+<p><h3>Tweet Master Advanced Version:</h3></p>
+<p>Display or hide Widget Title, Display Tweet Button with bubble count, Display your latest Tweets, Shortcode Framework, publish widget inside pages and posts.</p>
+<p><a class="button-primary" href="http://wordpress.techgasp.com/tweet-master/" target="_blank" title="Tweet Master Advanced Version">Tweet Master Advanced Version</a></p>
+</form>
+</div>

a84140584e46ce006f0bdb64bc380e1680791e89
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-11 21:43:29 +0000

    version 4.0.1
    git-svn-id: https://plugins.svn.wordpress.org/tweet-master/trunk@802748 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: Techgasp
 Tags: wordpress, plugin, widget, shortcode, techgasp, twitter, tweet, share, social, embedded tweet, latest tweets, twitter list
 Requires at least: 3.5
-Tested up to: 3.7
-Stable tag: 4.0
+Tested up to: 3.7.1
+Stable tag: 4.0.1
 License: GPLv2 or later
 
 With Tweet Master plugin you can display your latest tweets, favourite twitter lists and tweet button.
@@ -93,6 +93,9 @@ IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX.
 
 
 == Changelog ==
+
+= 4.0.1 =
+* Changing directory calls to support Multi-Domains wordpress installations
 
 = 4.0 =
 * Implementing New Shortcode Framework v2. Shortcode box will now be available when editing a page or a post
--- tweet-master.php
+++ tweet-master.php
@@ -2,7 +2,7 @@
 /**
 Plugin Name: Tweet Master
 Plugin URI: http://wordpress.techgasp.com/tweet-master/
-Version: 4.0
+Version: 4.0.1
 Author: TechGasp
 Author URI: http://wordpress.techgasp.com
 Text Domain: tweet-master
@@ -34,22 +34,13 @@ define('TWEET_MASTER_ID', 'tweet-master');
 define('TWEET_MASTER_NICK', 'Tweet Master');
 
 // HOOK WIDGET
-require_once('includes/tweet-master-widget.php');
+require_once( dirname( __FILE__ ) . '/includes/tweet-master-widget.php');
 
 // HOOK INVITATION
 
 // HOOK SHORTCODE
 
 	class tweet_master{
-		/** function/method
-		* Usage: return absolute file path
-		* Arg(1): string
-		* Return: string
-		*/
-		public static function file_path($file)
-		{
-			return ABSPATH.'wp-content/plugins/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)).$file;
-		}
 		/** function/method
 		* Usage: hooking the plugin options/settings
 		* Arg(0): null
@@ -83,7 +74,7 @@ require_once('includes/tweet-master-widget.php');
 			}
 			$plugin_id = TWEET_MASTER_ID;
 			// display options page
-			include(self::file_path('includes/tweet-master-admin.php'));
+			include( dirname( __FILE__ ) . '/includes/tweet-master-admin.php');
 		}
 		/** function/method
 		* Usage: show options/settings form page
@@ -93,7 +84,7 @@ require_once('includes/tweet-master-widget.php');
 		 public static function tweet_master_widget()
 		{
 			// display widget page
-			include(self::file_path('includes/tweet-master-widget.php'));
+			include( dirname( __FILE__ ) . '/includes/tweet-master-widget.php');
 		}
 		/** function/method
 		* Usage: filtering the content
@@ -121,7 +112,6 @@ require_once('includes/tweet-master-widget.php');
 		{
 		add_action('admin_init', array('tweet_master', 'tweet_master_register'));
 		add_action('admin_menu', array('tweet_master', 'menu'));
-
 		}
 	add_filter('the_content', array('tweet_master', 'content_with_quote'));
 endif;

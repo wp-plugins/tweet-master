897d4d3a394eef46bcdd4110147cfe8b5f766e13
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 00:21:12 +0000

    version 4.0.2
    git-svn-id: https://plugins.svn.wordpress.org/tweet-master/tags/4.0.2@820551 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/tweet-master-widget.php
@@ -0,0 +1,103 @@
+<?php
+//Hook Widget
+add_action( 'widgets_init', 'tweet_master_widget' );
+//Register Widget
+function tweet_master_widget() {
+register_widget( 'tweet_master_widget' );
+}
+
+class tweet_master_widget extends WP_Widget {
+	function tweet_master_widget() {
+	$widget_ops = array( 'classname' => 'Tweet Master', 'description' => __('With Tweet Master plugin you can display your latest tweets, favorite twitter lists and tweet button inside any widget position of your wordpress template. ', 'tweet_master') );
+	$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'tweet_master_widget' );
+	$this->WP_Widget( 'tweet_master_widget', __('Tweet Master', 'tweet_master'), $widget_ops, $control_ops );
+	}
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+		//Our variables from the widget settings.
+		$name = "Tweet Master";
+		$title = isset( $instance['title'] ) ? $instance['title'] :false;
+		$tweetspacer ="'";
+		$show_tweetbutton = isset( $instance['show_tweetbutton'] ) ? $instance['show_tweetbutton'] :false;
+		$twitteruser = $instance['twitteruser'];
+		echo $before_widget;
+		
+		// Display the widget title
+	if ( $title )
+		echo $before_title . $name . $after_title;
+	//Display Latest Tweets
+
+	//Display Tweet Button
+	if ( $show_tweetbutton )
+			echo '<br/><a href="https://twitter.com/share" class="twitter-share-button" data-via="'.$twitteruser.'" data-related="'.$twitteruser.'" data-hashtags="'.$twitteruser.'">Tweet</a>' .
+				'<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'.$tweetspacer.'http'.$tweetspacer.':'.$tweetspacer.'https'.$tweetspacer.';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'.$tweetspacer.'://platform.twitter.com/widgets.js'.$tweetspacer.';fjs.parentNode.insertBefore(js,fjs);}}(document, '.$tweetspacer.'script'.$tweetspacer.', '.$tweetspacer.'twitter-wjs'.$tweetspacer.');</script>';
+		//Display Twitter Lists
+
+	echo $after_widget;
+	}
+	//Update the widget
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		//Strip tags from title and name to remove HTML
+		$instance['name'] = strip_tags( $new_instance['name'] );
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['show_tweetbutton'] = $new_instance['show_tweetbutton'];
+		$instance['twitteruser'] = $new_instance['twitteruser'];
+		return $instance;
+	}
+	function form( $instance ) {
+	//Set up some default widget settings.
+	$defaults = array( 'name' => __('Tweet Master', 'tweet_master'), 'title' => true, 'show_tweetbutton' => false, 'twitteruser' => false );
+	$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+		<b>Check the buttons to be displayed:</b>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['title'], true ); ?> id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><b><?php _e('Display Widget Title', 'tweet_master'); ?></b></label></br>
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<input type="checkbox" <?php checked( (bool) $instance['show_tweetbutton'], true ); ?> id="<?php echo $this->get_field_id( 'show_tweetbutton' ); ?>" name="<?php echo $this->get_field_name( 'show_tweetbutton' ); ?>" />
+	<label for="<?php echo $this->get_field_id( 'show_tweetbutton' ); ?>"><b><?php _e('Tweet Share Button', 'tweet_master'); ?></b></label></br>
+	</p>
+	<p>
+	<label for="<?php echo $this->get_field_id( 'twitteruser' ); ?>"><?php _e('insert your Twitter Username:', 'tweet_master'); ?></label></br>
+	<input id="<?php echo $this->get_field_id( 'twitteruser' ); ?>" name="<?php echo $this->get_field_name( 'twitteruser' ); ?>" value="<?php echo $instance['twitteruser']; ?>" style="width:auto;" />
+	</p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<b>Display Latest Tweets</b>
+	</p>
+	<div class="description">Only available in advanced version.</div><br>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+	<p>
+	<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" />
+	&nbsp;
+	<b>Display Twitter List</b>
+	</p>
+	<div class="description">Only available in advanced version.</div><br>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+		<p>
+		<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+		&nbsp;
+		<b>Tweet Master Website</b>
+		</p>
+		<p><a class="button-secondary" href="http://wordpress.techgasp.com/tweet-master/" target="_blank" title="Tweet Master Info Page">Info Page</a> <a class="button-secondary" href="http://wordpress.techgasp.com/tweet-master-documentation/" target="_blank" title="Tweet Master Documentation">Documentation</a> <a class="button-primary" href="http://wordpress.techgasp.com/tweet-master/" target="_blank" title="Tweet Master">Advanced Version</a></p>
+<div style="background: url(<?php echo plugins_url('../images/techgasp-hr.png', __FILE__); ?>) repeat-x; height: 10px"></div>
+		<p>
+		<img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; width:16px; vertical-align:middle;" />
+		&nbsp;
+		<b>Advanced Version Updater:</b>
+		</p>
+		<div class="description">The advanced version updater allows to automatically update your advanced plugin. Only available in advanced version.</div>
+	<?php
+	}
+ }
+?>
---
+++ readme.txt
@@ -0,0 +1,140 @@
+=== Tweet Master ===
+Contributors: Techgasp
+Tags: wordpress, plugin, widget, shortcode, techgasp, twitter, tweet, share, social, embedded tweet, latest tweets, twitter list
+Requires at least: 3.5
+Tested up to: 3.7.1
+Stable tag: 4.0.2
+License: GPLv2 or later
+
+With Tweet Master plugin you can display your latest tweets, favourite twitter lists and tweet button.
+
+== Description ==
+
+By far the best Twitter Tweets integration for Wordpress websites. With Tweet Master you can display your latest tweets and or your favourite Twitter lists inside any widget position of your wordpress template. Besides your tweets display we also included a nice tweet button with bubble count reinforced with Via, Recommend and Hashtag. Your users and visitors can easily share your page boosting your website visits and social engagement.
+
+Since we are obsessed with clean, error free code and page load times, we made sure the widget updates your tweets in a blink of an eye without stalling it if twitter is responding slowly. And now you ask, how did you achieve that? The plugin is based upon the twitter widget when it comes to sizes and colours but we packed the module with it's own iframe and html5.
+
+With Tweets Master you can professionally display your twitter tweets and integrate it into your wordpress like a chameleon.
+
+**Tweet Master boosts your social engagement**
+
+IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX.
+
+
+**LITE Version Contains:**
+
+* Display or hide Widget Title
+* Display or hide Tweet Button with bubble count
+
+**ADVANCED Version Contains:**
+
+* Display or hide Widget Title
+* Display or hide Tweet Button with bubble count
+* Display or hide your latest Tweets
+* Display or hide your favourite Twitter List
+* Shortcode Framework v2. Publish widget inside pages and posts
+* [Advanced Version Link](http://wordpress.techgasp.com/tweet-master/)
+
+**ONLINE DEMO**
+
+* Right of the page, sidebar widget
+* Inside the post, with shortcode framework v2
+* [Demo Link](http://wordpress.techgasp.com/tweet-master/)
+
+**More High Quality Plugins?**
+
+* For professional wordpress websites
+* Tested for fast page load times and SEO
+* Errors and conflicts free. NO JAVASCRIPT or AJAX
+* [Click Here](http://wordpress.techgasp.com/buy-all-techgasp-wordpress-plugins/)
+
+
+== Installation ==
+
+1. Use the Wordpress Installer, Updater.
+2. Activate in the "Plugins" admin panel.
+3. Visit the "Widget Page | Tweet Master" to configure the settings and your template widget position.
+4. [Tweet Master Documentation](http://wordpress.techgasp.com/tweet-master-documentation)
+
+
+== Frequently Asked Questions ==
+
+= Issues with buttons display =
+* Please update to latest version. Up-to-date API's are available
+
+= Where are my tweets? =
+* Advanced Version contains: Display or hide Widget Title, Display Tweet Button with bubble count, Display your latest Tweets, Display your favourite Twitter List, Shortcode Framework v2, publish widget inside pages and posts
+* [Advanced Version Link](http://wordpress.techgasp.com/tweet-master/)
+
+= Insert Widget in Pages or Posts =
+* Advanced Version contains the shortcode framework v2
+* Insert the shortcode into any page or post. [tweet-master]
+* [Advanced Version Link](http://wordpress.techgasp.com/tweet-master/)
+
+= What's this Shortcode Framework v2 =
+* It's a framework developed by TechGasp and built upon wordpress marvellous resources
+* Allows users to quickly and easily display plugins or widgets inside their pages and posts
+* It's available in all TechGasp advanced version plugins. Lite versions plugins do not include shortcode framework v2
+* [About Shortcode Framework v2](http://wordpress.techgasp.com/new-wordpress-framework-from-techgasp/)
+
+= Need More Help =
+* [Tweet Master Documentation](http://wordpress.techgasp.com/tweet-master-documentation)
+* [Support](http://wordpress.techgasp.com/support)
+
+
+== Screenshots ==
+
+1. Widget With latest Tweets Display (black color scheme) + Tweet Button. Sidebar Widget Area.
+2. Widget With latest Tweets Display (white color scheme). Sidebar Widget Area.
+3. Widget With favourite Twitter List Display (black color scheme) + Tweet Button. Sidebar Widget Area.
+4. Widget With latest Tweets Display (black color scheme) + favourite Twitter List Display (black color scheme) + Tweet Button. Sidebar Widget Area.
+5. Widget With latest Tweets Display (black color scheme) + Tweet Button. Footer Widget Area.
+6. Widget inside pages and posts. Shortcode framework v2 implementation. Latest Tweets Display inside a page, black color scheme.
+
+
+== Changelog ==
+
+= 4.0.2 =
+* Faster Background Updater
+
+= 4.0.1 =
+* Changing directory calls to support Multi-Domains wordpress installations
+
+= 4.0 =
+* Implementing New Shortcode Framework v2. Shortcode box will now be available when editing a page or a post
+
+= 3.5 =
+* Implementing advanced updater
+
+= 3.1 =
+* Added admin_notices
+* Removed blockquotes that seem to clash in inferior templates
+
+= 3.0 =
+* Added Twitter Lists Display
+
+= 2.0 =
+* Stable Release
+* Adding Shortcode Framework, advanced version only
+* Added option to display or hide widget title
+* Adding more screenshots
+
+= 1.0 =
+* Beta release
+* Adding Buttons
+* Start coding
+
+
+== Upgrade Notice ==
+
+= 4.0 =
+* IMPORTANT Upgrade to new TechGasp Plugin Framework
+
+= 3.0 =
+* IMPORTANT add-on released, Twitter Lists
+
+= 2.0 =
+* IMPORTANT Upgrade to Stable release
+
+= 1.0 =
+* IMPORTANT Beta Release, if you helped us test the Plugin, upgrade to Beta
